874696f6abdd99e84deeb84417c3f2186ada52ab
Arjur <zhangzhe.ace@foxmail.com>
2019-01-08 18:02:40 +0800

    z_blog structure and hello world.

---
+++ java/z_blog/z_blog_core/src/main/java/io/github/acezhe/zblog/ZblogApplication.java
@@ -0,0 +1,14 @@
+package io.github.acezhe.zblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZblogApplication.class, args);
+	}
+
+}
+
