1ca00075866db0f2335166017476551e83aac680
zhangzhe <zhangzhe.ace@foxmail.com>
2019-02-12 10:35:28 +0800

    finish springsecuritybasicdemo

---
+++ java/springsecuritybasicdemo/src/test/java/com/acezhe/springsecuritybasicdemo/SpringsecuritybasicdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.acezhe.springsecuritybasicdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringsecuritybasicdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
